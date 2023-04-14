477983676ac5d37b816be20d853598ced3965214
shotacular <rxkxj0225@gmail.com>
2023-04-14 17:56:01 +0900

    BootstrapVueとsass導入及びwatch実装

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,61 @@
     <!-- <button @click="$store.commit('increment')">
       {{ $store.state.counter }}
     </button> -->
+    <p>こちらはdefaultのlayout</p>
     <p>
-      ログイン状態1 ($store.getter)： {{ $store.getters['sample/getStatus'] }}
+      <span v-if="getStatus">ログイン状態1 ($store.getter)： </span>
+      {{ $store.getters['sample/getStatus'] }}
     </p>
-    <p>ログイン状態2 (getter)： {{ getStatus }}</p>
-    <p>ログイン状態3 (state)： {{ isStatus }}</p>
-    <button @click="$store.dispatch('sample/signIn')">
+    <p>
+      <span v-if="getStatus">ログイン状態2 (getter)： </span>
+      {{ getStatus }}
+    </p>
+    <p><span v-if="getStatus">ログイン状態3 (state)： </span> {{ isStatus }}</p>
+    <b-button class="test" @click="$store.dispatch('sample/signIn')">
       ログインする1 (actionのdispatch)
-    </button>
+    </b-button>
     <button @click="setStatus({ isLogin: true })">
       ログインする2 (mutation)
     </button>
-    <button @click="signIn">
+    <button @click="signIn(true)">
       ログインする3 (action)
     </button>
     <button @click="sayHello">
       ハロー
     </button>
-    <button @click="toggleAction">
+    <button @click="toggleAction()">
       トグル
     </button>
+    <div>
+      <b-button id="show-btn" @click="showModal">
+        Open Modal
+      </b-button>
+      <b-button id="toggle-btn" @click="toggleModal">
+        Toggle Modal
+      </b-button>
+
+      <b-modal ref="my-modal" hide-footer title="Using Component Methods">
+        <div class="d-block text-center">
+          <h3>Hello From My Modal!</h3>
+        </div>
+        <b-button
+          class="mt-3"
+          variant="outline-danger"
+          block
+          @click="hideModal"
+        >
+          Close Me
+        </b-button>
+        <b-button
+          class="mt-2"
+          variant="outline-warning"
+          block
+          @click="toggleModal"
+        >
+          Toggle Me
+        </b-button>
+      </b-modal>
+    </div>
   </div>
 </template>
 
@@ -41,16 +76,35 @@ export default {
     ...mapState('sample', ['isStatus']),
     ...mapGetters('sample', ['getStatus'])
   },
+  watch: {
+    isStatus(newVal, oldVal) {
+      // データの値が変化した時にコンソールに新しい値と古い値を出力
+      console.log(newVal, oldVal)
+    }
+  },
   methods: {
+    ...mapMutations('sample', ['setStatus']),
+    ...mapActions('sample', ['signIn', 'toggleAction']),
     sayHello() {
       console.log('Hello!')
     },
-    ...mapMutations({
-      setStatus: 'sample/setStatus'
-    }),
-    ...mapActions('sample', ['signIn', 'toggleAction'])
+    showModal() {
+      this.$refs['my-modal'].show()
+    },
+    hideModal() {
+      this.$refs['my-modal'].hide()
+    },
+    toggleModal() {
+      // We pass the ID of the button that we want to return focus to
+      // when the modal has hidden
+      this.$refs['my-modal'].toggle('#toggle-btn')
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.test {
+  color: red;
+}
+</style>
--- store/sample/actions.js
+++ store/sample/actions.js
@@ -1,8 +1,8 @@
 export default {
-  signIn({ commit }) {
+  signIn({ commit }, requestResult) {
     console.log('action到達')
     // ここでミューテーションを呼び出し、外部とのAPI通信や非同期処理も行う
-    commit('setStatus', { isLogin: true })
+    commit('setStatus', { isLogin: requestResult })
   },
   toggleAction({ commit }) {
     console.log('action到達')
